a1fd2012d51b6544761200cef611505080f0c6d9
abusou7 <112393861+abusou7@users.noreply.github.com>
2024-11-30 21:37:56 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleForms() {
+  const loginForm = document.querySelector('.login');
+  const signupForm = document.querySelector('.signup');
+  loginForm.classList.toggle('hide');
+  signupForm.classList.toggle('hide');
+}
